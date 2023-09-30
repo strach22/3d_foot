e330d40125e82ae29870331de08388fecb6f676c
Isaac Pozo Urgilés <isaacandrespozo@gmail.com>
2023-09-29 23:17:51 -0500

    redistribución

--- src/basic/Scene.js
+++ src/basic/Scene.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xF5F5DC);
+scene.background = new THREE.Color(0xF5F5DC);
 // scene.background = new THREE.TextureLoader().load("./src/images/burbujas.jpg")
 export default scene;
