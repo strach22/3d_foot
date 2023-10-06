2e4b941c051c92302ed903cd84885da2e6f42197
Isaac Pozo Urgilés <isaacandrespozo@gmail.com>
2023-10-05 23:21:03 -0500

    parametros

--- src/index.js
+++ src/index.js
@@ -10,11 +10,25 @@ import light from "./basic/Light";
 import coordenadas from "./basic/Coordenadas";
 import patologias from "./basic/Patologias";
 
-scene.add(light);
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return false;
+}
 
+const arr = JSON.parse(getQueryVariable("arr"));
 const loader = new GLTFLoader();
+const coordenadasArr = Object.keys(coordenadas);
 let pie;
-let cilindro;
+let shape;
+
+scene.add(light);
 
 loader.load("./resources/shapes/pies.gltf", function (gltf) {
   pie = gltf.scene;
@@ -23,15 +37,6 @@ loader.load("./resources/shapes/pies.gltf", function (gltf) {
   scene.add(pie);
 });
 
-Object.keys(coordenadas).forEach((e,i) => {
-  loader.load("./resources/shapes/"+patologias[i%patologias.length], function (gltf) {
-    cilindro = gltf.scene;
-
-    scene.add(cilindro);
-    cilindro.position.set(...coordenadas[e]);
-    cilindro.rotation.x += coordenadas[e][3];
-  });
-});
 const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.set(0, 0, 280);
@@ -44,3 +49,34 @@ function animate() {
 animate();
 
 resize.start(renderer);
+
+if (Array.isArray(arr) && arr.length > 0) {
+  arr.forEach((item) => {
+    if (item[0] < 0 || item[1] < 0 || item[0] >= coordenadasArr.length) return;
+    const ubic = coordenadasArr[item[0]];
+    const patho = patologias[item[1]];
+    loader.load("./resources/shapes/" + patho, function (gltf) {
+      shape = gltf.scene;
+
+      scene.add(shape);
+      shape.position.set(...coordenadas[ubic]);
+      shape.rotation.x += coordenadas[ubic][3];
+      if (item[0] < 8) {
+        shape.scale.set(3, 3, 3);
+      }
+    });
+  });
+} else {
+  coordenadasArr.forEach((e, i) => {
+    loader.load(
+      "./resources/shapes/" + patologias[i % patologias.length],
+      function (gltf) {
+        shape = gltf.scene;
+
+        scene.add(shape);
+        shape.position.set(...coordenadas[e]);
+        shape.rotation.x += coordenadas[e][3];
+      }
+    );
+  });
+}
