156aae18dfac238dd8560363c6206ea57d373493
Isaac Pozo Urgilés <isaacandrespozo@gmail.com>
2023-10-12 23:06:03 -0500

    img en legend

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ if (Array.isArray(arr) && arr.length > 0) {
     if (item[0] < 0 || item[1] < 0 || item[0] >= coordenadasArr.length) return;
     const ubic = coordenadasArr[item[0]];
     const patho = patologias[item[1]];
-    $('#legend').append("<table width='100%'><td align='center'>" + patho.replaceAll('_',' ').replace(".gltf","") + "</td><td align='center'>" + patho.replaceAll('_',' ').replace(".gltf","") + "</td></table>"); 
+    $('#legend').append("<table width='100%'><td align='center'><img src='" + patho.replaceAll('_',' ').replace(".gltf",".img") + "'></img></td><td align='center'>" + patho.replaceAll('_',' ').replace(".gltf","") + "</td></table>"); 
     $('#legend').show()
     loader.load("./resources/shapes/" + patho, function (gltf) {
       shape = gltf.scene;
